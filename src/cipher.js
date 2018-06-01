630c29395d19c4e9f02cbbdbfd93684cecae41a7
Fanny <fannybecerravera@gmail.com>
2018-05-31 21:28:48 -0400

    Arreglando ultimos detalles viendo identacion

--- src/cipher.js
+++ src/cipher.js
@@ -4,8 +4,8 @@ window.cipher = {
       for(let i = 0; i < str.length; i++) { //arreglo de caracteres
         if(65 <= str.charCodeAt(i) && str.charCodeAt(i) <= 90){ //Condicion para mayúsculas.Traducira al número de la letra en el Código ASCII
   	        let msgEncode = (((str.charCodeAt(i) - 65 + parseInt(offset)) % 26) + 65); //Se agrega el número de posiciones y nos da el número de código ASCII
-          encodeResult += String.fromCharCode (msgEncode);//Pasando el mensaje de número a letra
-  	    } else { 
+              encodeResult += String.fromCharCode (msgEncode);//Pasando el mensaje de número a letra
+  	    }else { 
   	      encodeResult += str.charAt(i)}; //En el caso de aparecer espacios o otro signo
   	  }
   	     return encodeResult;
@@ -16,10 +16,10 @@ window.cipher = {
       for(let i = 0; i < str.length; i++) {
         if(65 <= str.charCodeAt(i) && str.charCodeAt(i) <= 90){
             let msgEncode = (((str.charCodeAt(i) + 65 - parseInt(offset)) % 26) + 65); //Se resta el número de posiciones y nos da el número de código
-          decodeResult += String.fromCharCode (msgEncode);
-        } else { 
+            decodeResult += String.fromCharCode (msgEncode);
+        }else { 
           decodeResult += str.charAt(i)}; 
-          }
-             return decodeResult;
- }
+      }
+          return decodeResult;
+  }
 }
